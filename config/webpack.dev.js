efa1c732ca0260205ea8002fd3e094c6e019c35b
Nadav Sinai <ns@nadavsinai.com>
2016-09-01 16:42:16 +0300

    (feature) testing - testing infrastructure + test for chatService

---
+++ config/webpack.dev.js
@@ -0,0 +1,29 @@
+const defaultConfig = require('./webpack.common');
+const webpackMerge = require('webpack-merge');
+const webpack = require('webpack');
+
+// Webpack dev Config
+const webpackConfig = {
+	plugins: [new webpack.optimize.OccurenceOrderPlugin(true),
+		new webpack.optimize.CommonsChunkPlugin({name: ['app', 'vendor', 'polyfills'], minChunks: Infinity}),],
+	entry: {
+		'polyfills': './src/polyfills.ts',
+		'vendor': './src/vendor.ts',
+		'main': './src/main.ts'
+	},
+	output: {
+		path: './dist'
+	},
+	module: {
+		preLoaders: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				loader: "tslint"
+			}
+		]
+	}
+};
+
+
+module.exports = webpackMerge(defaultConfig, webpackConfig);

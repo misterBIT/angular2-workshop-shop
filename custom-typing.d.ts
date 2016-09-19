f68e2e097219d588b4e66a622af89671eb8c66ce
nadavsinai <ns@nadavsinai.com>
2016-09-19 23:08:31 +0300

    repo-reset - remove the example app from here also remove relevant dependencies to make this seed more bare

--- custom-typing.d.ts
+++ custom-typing.d.ts
@@ -1,12 +1,1 @@
-declare module 'simulant' {
-
-	namespace Simulant {
-		interface simulant {
-			(event: string, data: any): Event;
-			fire(target: Node, event: Event);
-		}
-		function simulant(event: string, data: any): Event;
-	}
-	export = Simulant.simulant;
-}
-
+//declare your untyped modules here
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,15 @@ import {NgModule} from "@angular/core";
 import {HttpModule} from "@angular/http";
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 
-import {ToastModule} from 'ng2-toastr';
-import {FILE_UPLOAD_DIRECTIVES} from "ng2-file-upload";
 import {CommonModule} from "@angular/common";
-let sharedModules = [CommonModule, FormsModule, ReactiveFormsModule, HttpModule, ToastModule];
+let sharedModules = [CommonModule, FormsModule, ReactiveFormsModule, HttpModule];
 let commonPipes = [];
-let commonDirectives = [FILE_UPLOAD_DIRECTIVES];
+let commonDirectives = [];
 
 @NgModule({
-	declarations: [...commonPipes, ...commonDirectives],
-	imports: sharedModules,
-	exports: [...commonDirectives, ...commonPipes, ...sharedModules]
+    declarations: [...commonPipes, ...commonDirectives],
+    imports: sharedModules,
+    exports: [...commonDirectives, ...commonPipes, ...sharedModules]
 })
 export class SharedModule {
 }

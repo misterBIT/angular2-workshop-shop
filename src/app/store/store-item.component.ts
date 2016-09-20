43b1e1fb7d9efe62c734625bd7a4c8116926aa0c
Nadav Sinai <ns@nadavsinai.com>
2016-09-20 23:55:37 +0300

    little more progress item Id

--- src/app/store/store-item.component.ts
+++ src/app/store/store-item.component.ts
@@ -1,15 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {StoreService, IStoreItem} from "./store.service";
 
 @Component({
 	selector: 'store-item',
-	templateUrl: `<h1> Item</h1>`
+	template: `<h1>{{item?.title}}</h1>
+			   <h3>{{item?.price}}</h3>`
 })
 export class StoreItemComponent implements OnInit {
-	constructor(private route: ActivatedRoute) {
+	private item: IStoreItem;
+
+	constructor(private route: ActivatedRoute, private store: StoreService) {
 	}
 
 	ngOnInit() {
+		this.store.getItem(this.route.snapshot.params['id']).then((item)=> {
+			this.item = item;
+		});
 	}
-
 }
